f1737290ed9bbd112d4efc549043572d531b6b4c
erickmo <erick.mo@vortege.com>
2015-03-17 08:36:20 +0700

    update composer.json

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInita2362e69792354e5152336dd5f5d052b::getLoader();
+return ComposerAutoloaderInit0db4b00fac677aad71b73c1adacd21bb::getLoader();
